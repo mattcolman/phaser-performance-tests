d64f1319676d3cad5ad07302df1b87a969f5d826
Matt Colman <matt.colman@fantasycentre.com>
2016-04-15 15:54:47 +1000

    draw 50 circles into the cache

--- src/states/spritesheet_builder_test.js
+++ src/states/spritesheet_builder_test.js
@@ -33,16 +33,44 @@ class SpriteSheetBuilderTest extends GameState {
 
     let ssb = new SpriteSheetBuilder(this.game)
     ssb.addFrames([['square', squareImage], ['circle', circleImage]])
-    ssb.buildToCache('new-atlas')
 
     let im = this.game.add.image(0, 0, ssb.bmd)
 
+    this.circles = _.times(50, ()=> {
+      return this.addCircle(50, 10)
+    })
+
+    var i = 0
+    ssb.addFrames(
+      _.map(this.circles, (circle)=> {
+        return [`circle${i++}`, circle.texture]
+      })
+    )
+
+    ssb.buildToCache('new-atlas')
     // let sp = this.game.add.sprite(100, 100, 'avatar')
     // let squareSprite = this.game.add.sprite(100, 100, 'new-atlas', 'square')
     // let circleSprite = this.game.add.sprite(300, 300, 'new-atlas', 'circle')
     // squareSprite.anchor.set(.5)
     // TweenMax.to(squareSprite, 1, {angle: 360, repeat: -1})
     // let img = this.game.add.image(100, 100, 'new-sprite')
+  }
+
+  addCircle(r, thickness) {
+    let w = r*2 + thickness
+    let bmd = this.game.add.bitmapData(w, w)
+    let c = bmd.ctx
+    let offset = 0
+
+    c.setLineDash([2, 4])
+    c.lineWidth = thickness
+    c.beginPath()
+    c.arc(w/2, w/2, r, offset, offset + (Phaser.Math.PI2-offset*2) * 1)
+    c.strokeStyle = Phaser.Color.getWebRGB(Phaser.Color.getRandomColor())
+    c.stroke()
+    c.closePath()
+
+    return bmd
   }
 
 }
